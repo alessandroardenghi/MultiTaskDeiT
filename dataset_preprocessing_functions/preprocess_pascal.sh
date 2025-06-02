bd882c95f71e0f1801e5454ccc20103f0a274c21
alessandroardenghi <alessandro.ardenghi.aa@gmail.com>
2025-06-02 13:35:53 +0200

    removed Slurm details

--- dataset_preprocessing_functions/preprocess_pascal.sh
+++ dataset_preprocessing_functions/preprocess_pascal.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-#SBATCH -c 16
-#SBATCH --mem=5G
-#SBATCH -p dsba
-#SBATCH -J setting_up_dir
-#SBATCH -e _err/err-%j.err
-#SBATCH -o _out/out-%j.out
-#SBATCH --gpus=0
-
-source /home/3144656/miniconda3/bin/activate
-
-conda activate jigsaw
-
 python3 dataset_preprocessing_functions/PascalVOC/download_pascal.py
 
 python3 dataset_preprocessing_functions/PascalVOC/preprocess_pascal.py \
